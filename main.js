ffe3db99239c5ba0da698a9b410aa6cca2f27c36
redscrapeshow <84358430+redscrapeshow@users.noreply.github.com>
2021-05-18 19:40:15 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 const f = async (x, y) => new Promise(resolve => setTimeout(_ => resolve(x), y))
 window.addEventListener('load', async function() {
-  console.log(await f('World!', 2000))
-  console.log(await f('Hello', 1000))
+  console.log(await f('Hello World!', 2000))
 })
